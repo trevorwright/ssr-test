0152909f39c9992960ff9f1e22830c8e9691bb45
Trevor Wright <trevor.wright@prodigygame.com>
2018-03-10 10:42:11 -0500

    Adds compression

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const next = require('next');
+const compression = require('compression');
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
@@ -11,6 +12,7 @@ app
   .prepare()
   .then(() => {
     const server = express();
+    server.use(compression());
 
     server.get('/p/:id', (req, res) => {
       const actualPage = '/post';
